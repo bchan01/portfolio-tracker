bca71790bc2b15944f06502c5315c3464716eb31
Brian Chan <brian_chan@cable.comcast.com>
2017-01-02 10:35:22 -0500

    Increase JWT expiration to 30 minutes

--- user-management-api/config/config.js
+++ user-management-api/config/config.js
@@ -8,7 +8,7 @@ module.exports = {
         'tokenHeaderName' : 'x-access-token',
     	'secret' : 'MySuperSecretKey',
         'issuer': 'b2mcomputing',
-        'timeout': 600,  // 10 minutes
+        'timeout': 1800,  // 30 minutes - 30*60
         'audience': 'PortfolioTracker',
         'subject' : 'noreply@b2mcomputing.com',
         'saltRounds' : 10,

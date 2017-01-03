8b210093b1e0e3bb7f35a8c02ee2ffb7929be4f4
Brian Chan <brian_chan@cable.comcast.com>
2017-01-02 20:51:37 -0500

    move stock quote fields to config

--- finance-api/config/config.js
+++ finance-api/config/config.js
@@ -6,7 +6,11 @@ module.exports = {
         'requestTimeout' : 10000,
         'quoteUrl' : 'http://finance.yahoo.com/d/quotes.csv',
         'histQuoteUrl' : 'http://ichart.yahoo.com/table.csv',
-        'chartUrl' : 'http://chart.finance.yahoo.com/z'
+        'chartUrl' : 'http://chart.finance.yahoo.com/z',
+         'quoteFields' : {
+            'keys' : 'snxl1d1t1baopc1p2hgkjv',
+            'names' : 'symbol,name,Stock Exchange,price,date,time,bid,ask,open,previousClose,change,changePercent,daysHigh,daysLow,high52Weeks,low52Weeks,volume'
+        }
     }
 
 };

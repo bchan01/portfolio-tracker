7eb60bcd2c476dea1222db21efccac0b991a3c6e
Brian Chan <brian_chan@cable.comcast.com>
2017-01-14 20:08:17 -0500

    Yeoman generated app

---
+++ portfolio-tracker-app/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name portfolioTrackerAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the portfolioTrackerAppApp
+ */
+angular.module('portfolioTrackerAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

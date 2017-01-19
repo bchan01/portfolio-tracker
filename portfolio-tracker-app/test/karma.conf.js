1b99aa0fdd0e7e6d31b5575d78fc82bcb71d6bd3
Brian Chan <brian_chan@cable.comcast.com>
2017-01-18 20:33:30 -0500

    no message

--- portfolio-tracker-app/test/karma.conf.js
+++ portfolio-tracker-app/test/karma.conf.js
@@ -29,6 +29,7 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
+      'bower_components/angular-jwt/dist/angular-jwt.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',

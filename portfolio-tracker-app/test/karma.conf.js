8408d2e0edb240f1ed91b4d9d4a0b5336787d0bf
brian chan <brian.chan.01@gmail.com>
2017-01-24 21:28:52 -0500

    add angular-bootstrap

--- portfolio-tracker-app/test/karma.conf.js
+++ portfolio-tracker-app/test/karma.conf.js
@@ -23,6 +23,7 @@ module.exports = function(config) {
       'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
       'bower_components/bootstrap/dist/js/bootstrap.js',
+      'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
       'bower_components/angular-animate/angular-animate.js',
       'bower_components/angular-cookies/angular-cookies.js',
       'bower_components/angular-resource/angular-resource.js',

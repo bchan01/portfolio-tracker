0137bf2609279f7647a5ec6a7b5e0d22b6353b86
brian chan <brian.chan.01@gmail.com>
2017-01-26 13:10:39 -0500

    Add flow for adding and deleting holding.  Need to call service to perform actual update in data store

--- portfolio-tracker-app/app/scripts/controllers/portfolio.js
+++ portfolio-tracker-app/app/scripts/controllers/portfolio.js
@@ -2,31 +2,77 @@
 angular.module('portfolioTrackerApp').controller('PortfolioController', PortfolioController);
 
 function PortfolioController($http, portfolioService) {
-  var vm = this;
-  vm.portfolios = [];
-  vm.message = null;
-  portfolioService.getPortfolios().then(function(response) {
-    console.log(response);
-    if (response.status === 200) {
-        vm.portfolios = response.data;
-    } else {
-        vm.portfolios = [];
-        vm.message = response.message;
-    }
-  });
+    var vm = this;
+    vm.portfolios = [];
+    vm.holdings = [];
+    vm.message = null;
+    vm.portfolio = null;
 
-  vm.getHoldings = function() {
-      vm.holdings = [];
-      vm.message = null;
-      console.log('getHoldings for portfolio: ' + vm.portfolio.id);
-      portfolioService.getHoldings(vm.portfolio.id).then(function(response) {
+    portfolioService.getPortfolios().then(function(response) {
+        console.log('PortfolioController.getPortfolios()');
+        console.log(response);
         if (response.status === 200) {
-            vm.holdings = response.data
+            vm.portfolios = response.data;
+            console.log('portfolio found - count:' + vm.portfolios.length);
         } else {
-            vm.holdings = [];
+            vm.portfolios = [];
             vm.message = response.message;
+            console.log('portfolio not found - count:' + vm.portfolios.length);
         }
     });
- }
-  
+
+    /**
+     * Load Holdings for selected portfolio
+     */
+    vm.getHoldings = function() {
+        vm.holdings = [];
+        vm.message = null;
+        console.log('getHoldings for portfolio: ' + vm.portfolio.id);
+        portfolioService.getHoldings(vm.portfolio.id).then(function(response) {
+            if (response.status === 200) {
+                vm.holdings = response.data
+            } else {
+                vm.holdings = [];
+                vm.message = response.message;
+            }
+        });
+    }
+
+    /**
+     * Delete selected holding
+     */
+    vm.removeHolding = function(holdingId) {
+        vm.message = null;
+        console.log('removeHolding for id: ' + holdingId);
+        var index = -1;		
+        for( var i = 0; i < vm.holdings.length; i++ ) {
+            if( vm.holdings[i]._id === holdingId ) {
+                index = i;
+                break;
+            }
+        }
+        if( index === -1 ) {
+            alert( "Something gone wrong" );
+        }
+        vm.holdings.splice( index, 1 );	
+    }
+
+    /**
+     * Add new holding to a portfolio
+     */
+    vm.addHolding = function(result) {
+        vm.message = null;
+        var holding = {
+            'symbol' : vm.symbol,
+            'description' :  vm.description,
+            'shares' : vm.shares,
+            'purchasePrice' : vm.purchasePrice,
+            'tradeDate' : vm.tradeDate,
+            'commission' : vm.commission
+        }
+        console.log('addHolding: ' + JSON.stringify(holding));
+        vm.holdings.push(holding);
+    }
+
+
 }
